317eae68a2c572683942c718f36ea54f25c72727
kesh-hrishi <hrishikeshajith0@gmail.com>
2023-05-22 18:59:58 +0530

    Inintial homepage commit

---
+++ lib/loginpage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatelessWidget {
+  const Login({super.key});
+
+  @override
+  Widget build(context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello"),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/splashscreen.dart
@@ -0,0 +1,52 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:echno_attendance/loginpage.dart';
+
+class Splashpage extends StatefulWidget {
+  const Splashpage({super.key});
+
+  @override
+  State<Splashpage> createState() => _SplashpageState();
+}
+
+class _SplashpageState extends State<Splashpage> {
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(Duration(seconds: 2), () {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Login(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Image.asset(
+                'assets/logo/2.png',
+                height: 200,
+                width: 200,
+              ),
+              Image.asset(
+                'assets/logo/3.png',
+                height: 150,
+                width: 150,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
