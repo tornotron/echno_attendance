7bd673440f7849d387b00381c9a7420ab1ff7684
kesh-hrishi <hrishikeshajith0@gmail.com>
2023-05-27 02:43:53 +0530

    added company footer img

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -29,23 +29,29 @@ class _SplashpageState extends State<Splashpage> {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Image.asset(
-                'assets/logo/2.png',
-                height: 200,
-                width: 200,
-              ),
-              Image.asset(
-                'assets/logo/3.png',
-                height: 150,
-                width: 150,
-              ),
-            ],
+        body: Stack(children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Image.asset(
+                  'assets/logo/2.png',
+                  height: 200,
+                  width: 200,
+                ),
+                Image.asset(
+                  'assets/logo/3.png',
+                  height: 150,
+                  width: 150,
+                ),
+              ],
+            ),
           ),
-        ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Image.asset('assets/logo/footer.png'),
+          ),
+        ]),
       ),
     );
   }
