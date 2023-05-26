0d5297cb4a4e1f3a9b6ac09ff8660461083fa2b6
kesh-hrishi <hrishikeshajith0@gmail.com>
2023-05-27 02:43:53 +0530

    added company footer img

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -5,10 +5,63 @@ class Login extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const MaterialApp(
+    Size size = MediaQuery.of(context).size;
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello"),
+        body: Container(
+          width: double.infinity,
+          height: size.height,
+          child: Stack(
+            children: <Widget>[
+              Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [Image.asset(
+                  'assets/logo/2.png',
+                  height: 100,
+                  width: 100,
+                ),
+                Image.asset(
+                  'assets/logo/3.png',
+                  height: 75,
+                  width: 75,
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Login",
+                  style: TextStyle(
+                    fontFamily: 'TT Chocolates',
+                    fontSize: 40,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const SizedBox(
+                  height: 5,
+                ),
+                const Text(
+                  "Sign in to continue.",
+                  style: TextStyle(
+                    fontFamily: 'TT Chocolates',
+                    color: Colors.grey,
+                    fontSize: 14,
+                  ),
+                ),
+                
+                
+                
+                ],
+                ),
+              ),
+              
+
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Image.asset('assets/logo/footer.png'),
+              ),
+            ],
+          ),
         ),
       ),
     );
