9d5248f1fac804528866067d0dd78347d3b72f1d
kesh-hrishi <hrishikeshajith0@gmail.com>
2023-06-01 11:06:28 +0530

    Navigation bar (test)

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -9,10 +10,11 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           systemOverlayStyle:
-              SystemUiOverlayStyle(statusBarColor: Color(0xFF004AAD)),
+              const SystemUiOverlayStyle(statusBarColor: Color(0xFF004AAD)),
           leading: const Icon(
             Icons.account_circle_rounded,
             size: 35,
@@ -21,17 +23,54 @@ class HomePage extends StatelessWidget {
             Center(
               child: Text(
                 appbarUserName,
-                style: TextStyle(
+                style: const TextStyle(
                     fontFamily: 'TT Chocolates',
                     fontSize: 25,
                     fontWeight: FontWeight.w700),
               ),
             ),
-            SizedBox(
+            const SizedBox(
               width: 10,
             )
           ],
           backgroundColor: const Color(0xFF004AAD),
+        ),
+        bottomNavigationBar: CurvedNavigationBar(
+          height: 60,
+          backgroundColor: Colors.white,
+          color: Color(0xFF004AAD),
+          items: [
+            Container(
+              child: Image.asset(
+                'assets/icons/5.png',
+                scale: 50,
+              ),
+            ),
+            Container(
+              child: Image.asset(
+                'assets/icons/7.png',
+                scale: 50,
+              ),
+            ),
+            Container(
+              child: Image.asset(
+                'assets/icons/10.png',
+                scale: 50,
+              ),
+            ),
+            Container(
+              child: Image.asset(
+                'assets/icons/11.png',
+                scale: 50,
+              ),
+            ),
+            Container(
+              child: Image.asset(
+                'assets/icons/13.png',
+                scale: 50,
+              ),
+            ),
+          ],
         ),
       ),
     );
