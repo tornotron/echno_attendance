c9e83ce08b31b99b9466e41bf1c2dcab0f83658c
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-06-05 00:11:50 +0530

    Basic user registration widget added

--- lib/echno.dart
+++ lib/echno.dart
@@ -1,3 +1,6 @@
+import 'package:echno_attendance/firebase_options.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class EchnoApp extends StatelessWidget {
@@ -17,8 +20,30 @@ class EchnoApp extends StatelessWidget {
   }
 }
 
-class EchnoHomePage extends StatelessWidget {
+class EchnoHomePage extends StatefulWidget {
   const EchnoHomePage({super.key});
+
+  @override
+  State<EchnoHomePage> createState() => _EchnoHomePageState();
+}
+
+class _EchnoHomePageState extends State<EchnoHomePage> {
+  late final TextEditingController _emailController;
+  late final TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    _emailController = TextEditingController();
+    _passwordController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +51,42 @@ class EchnoHomePage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Echno'),
       ),
-      body: const Center(
-        child: Text(
-          'Echno',
-          style: TextStyle(fontSize: 24),
-        ),
+      body: Column(
+        children: [
+          TextField(
+            controller: _emailController,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              labelText: 'Email',
+            ),
+          ),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            enableSuggestions: false,
+            autocorrect: false,
+            decoration: const InputDecoration(
+              labelText: 'Password',
+            ),
+          ),
+          TextButton(
+            onPressed: () async {
+              await Firebase.initializeApp(
+                options: DefaultFirebaseOptions.currentPlatform,
+              );
+              final email = _emailController.text;
+              final password = _passwordController.text;
+              final userCredential =
+                  await FirebaseAuth.instance.createUserWithEmailAndPassword(
+                email: email,
+                password: password,
+              );
+              print(userCredential);
+            },
+            child: const Text('Register'),
+          ),
+        ],
       ),
     );
   }
