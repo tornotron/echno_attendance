f503e21e9a03b3d97761bda0b8e2d7127ccec53c
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-06-07 12:05:40 +0530

    Added a HomPageView for logged in users

---
+++ lib/screens/homepage_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePageView extends StatefulWidget {
+  const HomePageView({super.key});
+
+  @override
+  State<HomePageView> createState() => _HomePageViewState();
+}
+
+class _HomePageViewState extends State<HomePageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Echno"),
+      ),
+      body: const Center(
+        child: Text("Welcome to Echno!"),
+      ),
+    );
+  }
+}
