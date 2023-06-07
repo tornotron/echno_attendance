c12bab8b8dbda1678764363d3be4e9aa88b97f07
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-06-07 12:23:43 +0530

    Added a Logout MenuAction button inside HomPageView AppBar

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class HomePageView extends StatefulWidget {
   const HomePageView({super.key});
@@ -7,12 +8,29 @@ class HomePageView extends StatefulWidget {
   State<HomePageView> createState() => _HomePageViewState();
 }
 
+enum MenuActions { logout }
+
 class _HomePageViewState extends State<HomePageView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Echno"),
+        actions: [
+          PopupMenuButton<MenuActions>(
+            onSelected: (value) {
+              devtools.log('Logged out!');
+            },
+            itemBuilder: (context) {
+              return const [
+                PopupMenuItem(
+                  value: MenuActions.logout,
+                  child: Text("Logout"),
+                ),
+              ];
+            },
+          )
+        ],
       ),
       body: const Center(
         child: Text("Welcome to Echno!"),
