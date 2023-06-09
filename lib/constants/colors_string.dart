484dec141cb872235bbf26be8b57ed0e44db9c1c
Abin K Johny <abinkjohny2011@gmail.com>
2023-06-09 21:01:36 +0530

    Defined the colors used in the applications

---
+++ lib/constants/colors_string.dart
@@ -0,0 +1,18 @@
+/* ------------------ Theme Colors ------------------ */
+
+import 'package:flutter/material.dart';
+
+const echnoBlueColor = Color(0xFF004AAD);
+const echnoLightBlueColor = Color(0xFF38B6FF);
+const echnoRoseColor = Color(0xFFE46C6C);
+const echnoGreenColor = Color(0xFF45A834);
+const echnoWhiteColor = Color(0xFFD0DFD6);
+const echnoPinkColor = Color(0xFFF6AFAF);
+const echnoGreyColor = Color(0xFFA6A6A6);
+
+const echnoLightColor = Color(0xff000000);
+const echnoDarkColor = Colors.white;
+const echnoDarkSwatchColor = Color(0xFF5EE7E2);
+const echnoBlueLightColor = Color(0xFFC3E7FD);
+const whiteColor = Colors.white;
+const echnoLogoColor = Color(0xFF0A3774);
