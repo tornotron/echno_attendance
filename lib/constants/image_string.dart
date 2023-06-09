4598f28c668f75f9889db2d8912f07c0bd413fd1
Abin K Johny <abinkjohny2011@gmail.com>
2023-06-09 21:03:35 +0530

    Assigned the images used in the application to variables

---
+++ lib/constants/image_string.dart
@@ -0,0 +1,14 @@
+/* -- Image Strings Used in the App -- */
+
+const String echnoTLogo = 'assets/logo/T-Logo.svg';
+const String echnoElogo = 'assets/logo/E-Logo.svg';
+const String echnoLogo = 'assets/logo/echnologo.svg';
+
+const String echnoOnboarding = 'assets/images/onboarding.svg';
+const String echnoPassword = 'assets/images/forgot-password.svg';
+const String echnoOTP = 'assets/images/otp.svg';
+const String echnoWelcome = 'assets/images/welcome.svg';
+const String echnoRegister = 'assets/images/register.svg';
+const String echnoSignIn = 'assets/images/signin.svg';
+
+const String googleIcon = 'assets/icons/google.svg';
