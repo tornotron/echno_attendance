7a3deb58b8e3759d9c8bc354bc9e6835d13baccd
Abin K Johny <abinkjohny2011@gmail.com>
2023-06-09 23:26:22 +0530

    Created header for WelcomeScreen

---
+++ lib/user_authentication/screens/wecome_screen.dart
@@ -0,0 +1,53 @@
+import 'package:echno_attendance/constants/image_string.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+  static const EdgeInsetsGeometry containerPadding =
+      EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0);
+
+  @override
+  Widget build(context) {
+    final mediaQuery = MediaQuery.of(context);
+    final height = mediaQuery.size.height;
+
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            width: double.infinity,
+            height: height,
+            padding: containerPadding,
+            child: Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  /*-----------------Welcome Header Start -----------------*/
+
+                  SvgPicture.asset(
+                    echnoLogo,
+                  ),
+                  Text(
+                    'Welcome to Echno!',
+                    style: Theme.of(context).textTheme.displayMedium,
+                  ),
+                  const SizedBox(height: 15),
+                  Text(
+                    'Unlock the potential of effective attendance tracking and progress monitoring!',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+
+                  /*-----------------Welcome Header End -----------------*/
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
