47d67e3d41e6deb8d0ad364d47cb64ee0ca28cef
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-06-10 09:23:00 +0530

    Added an auth service to abstract out firebase auth from UI code

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,35 @@
+// Purpose: Login Exceptions
+
+class UserNotFoundAuthException implements Exception {
+  final String message = 'User not found';
+}
+
+class WrongPasswordAuthException implements Exception {
+  final String message = 'Wrong password';
+}
+
+// Register Exceptions
+
+class EmailAlreadyInUseAuthException implements Exception {
+  final String message = 'Email already in use';
+}
+
+class WeakPasswordAuthException implements Exception {
+  final String message = 'Weak password';
+}
+
+class InvalidEmailAuthException implements Exception {
+  final String message = 'Invalid email';
+}
+
+// Purpose: General Exceptions
+
+class GenericAuthException implements Exception {
+  final String message;
+
+  GenericAuthException(this.message);
+}
+
+class UserNotLoggedInAuthException implements Exception {
+  final String message = 'User not logged in';
+}
