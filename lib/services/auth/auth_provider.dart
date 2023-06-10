8b36cf6d08b2ad16c60cc37a635631b418e6bce1
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-06-10 16:41:32 +0530

    Added firebase initialization to the AuthService

--- lib/services/auth/auth_provider.dart
+++ lib/services/auth/auth_provider.dart
@@ -3,6 +3,8 @@ import 'package:echno_attendance/services/auth/auth_user.dart';
 abstract class AuthProvider {
   AuthUser? get currentUser;
 
+  Future<void> initialize();
+
   Future<AuthUser> logIn({
     required String email,
     required String password,
