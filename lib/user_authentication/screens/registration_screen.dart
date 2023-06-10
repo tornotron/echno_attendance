3925ff7c9f2aab2a061c80c387350fa57e7ce87d
Abin K Johny <abinkjohny2011@gmail.com>
2023-06-10 20:11:47 +0530

    Created header for RegistrationScreen

---
+++ lib/user_authentication/screens/registration_screen.dart
@@ -0,0 +1,56 @@
+import 'package:echno_attendance/constants/image_string.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class RegistrationScreen extends StatelessWidget {
+  const RegistrationScreen({super.key});
+  static const EdgeInsetsGeometry containerPadding =
+      EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0);
+
+  @override
+  Widget build(context) {
+    final mediaQuery = MediaQuery.of(context);
+    final height = mediaQuery.size.height;
+
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Container(
+              width: double.infinity,
+              height: height,
+              padding: containerPadding,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  /*---------- Register Screen Header Start ----------*/
+
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SvgPicture.asset(
+                        echnoRegister,
+                        height: height * 0.15,
+                      ),
+                      const SizedBox(height: 15.0),
+                      Text('Get On Board!',
+                          style: Theme.of(context).textTheme.displaySmall),
+                      Text(
+                        'Create an account to start your Journey ...',
+                        style: Theme.of(context).textTheme.titleMedium,
+                      ),
+                    ],
+                  ),
+
+                  /*---------- Register Screen Header End ----------*/
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
