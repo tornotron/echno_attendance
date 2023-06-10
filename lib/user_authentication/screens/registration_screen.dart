42829911fb006125c9c390723fa047f799fcc0bf
Abin K Johny <abinkjohny2011@gmail.com>
2023-06-11 01:12:42 +0530

    Completed registration form-section

--- lib/user_authentication/screens/registration_screen.dart
+++ lib/user_authentication/screens/registration_screen.dart
@@ -49,76 +49,86 @@ class RegistrationScreen extends StatelessWidget {
 
                   /*---------- Register Screen Form Start ----------*/
 
-                  const SizedBox(height: 15.0),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextFormField(
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(Icons.person_outline_outlined),
-                          labelText: 'Full Name',
-                          hintText: 'Full Name',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(
-                              (15.0),
+                  Form(
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(vertical: 20.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          TextFormField(
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              prefixIcon:
+                                  const Icon(Icons.person_outline_outlined),
+                              labelText: 'Full Name',
+                              hintText: 'Full Name',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(
+                                  (15.0),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      TextFormField(
-                        enableSuggestions: false,
-                        autocorrect: false,
-                        keyboardType: TextInputType.emailAddress,
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                          // filled: true,
-                          // fillColor:
-                          // const Color.fromARGB(255, 214, 214, 214),
-                          prefixIcon: const Icon(Icons.person_outline_outlined),
-                          labelText: 'Email ID',
-                          hintText: 'E-Mail',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(15.0),
+                          const SizedBox(height: 10.0),
+                          TextFormField(
+                            enableSuggestions: false,
+                            autocorrect: false,
+                            keyboardType: TextInputType.emailAddress,
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              // filled: true,
+                              // fillColor:
+                              // const Color.fromARGB(255, 214, 214, 214),
+                              prefixIcon:
+                                  const Icon(Icons.person_outline_outlined),
+                              labelText: 'Email ID',
+                              hintText: 'E-Mail',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      TextFormField(
-                        keyboardType: TextInputType.number,
-                        maxLines: 1,
-                        decoration: InputDecoration(
-                          prefixIcon: const Icon(Icons.phone_android_outlined),
-                          labelText: 'Phone Number',
-                          hintText: '+1-1234567890',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15.0)),
-                        ),
-                      ),
-                      const SizedBox(height: 10.0),
-                      const PasswordTextField(
-                        labelText: 'Password',
-                        hintText: 'Password',
-                      ),
-                      const SizedBox(height: 10.0),
-                      const PasswordTextField(
-                        labelText: 'Re-Enter Password',
-                        hintText: 'Re-Enter Password',
-                      ),
-                      const SizedBox(height: 30.0),
-                      SizedBox(
-                        width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {},
-                          child: const Text(
-                            'REGISTER',
-                            style: TextStyle(
-                                fontSize: 17.0, fontFamily: 'TT Chocolates'),
+                          const SizedBox(height: 10.0),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            maxLines: 1,
+                            decoration: InputDecoration(
+                              prefixIcon:
+                                  const Icon(Icons.phone_android_outlined),
+                              labelText: 'Phone Number',
+                              hintText: '+1-1234567890',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15.0)),
+                            ),
                           ),
-                        ),
+                          const SizedBox(height: 10.0),
+                          const PasswordTextField(
+                            labelText: 'Password',
+                            hintText: 'Password',
+                          ),
+                          const SizedBox(height: 10.0),
+                          const PasswordTextField(
+                            labelText: 'Re-Enter Password',
+                            hintText: 'Re-Enter Password',
+                          ),
+                          const SizedBox(height: 30.0),
+                          SizedBox(
+                            width: double.infinity,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'REGISTER',
+                                style: TextStyle(
+                                    fontSize: 17.0,
+                                    fontFamily: 'TT Chocolates'),
+                              ),
+                            ),
+                          ),
+
+                          /*---------- Register Screen Form End ----------*/
+                        ],
                       ),
-                    ],
+                    ),
                   ),
                 ],
               ),
