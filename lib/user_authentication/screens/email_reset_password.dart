a848bd6a1824ae04dddfb83588ca4cc428b768bb
Abin K Johny <abinkjohny2011@gmail.com>
2023-06-11 20:37:26 +0530

    Created email confirmation page for password-reset

---
+++ lib/user_authentication/screens/email_reset_password.dart
@@ -0,0 +1,77 @@
+import 'package:echno_attendance/constants/image_string.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class MailPasswordResetScreen extends StatelessWidget {
+  const MailPasswordResetScreen({super.key});
+  static const EdgeInsetsGeometry containerPadding =
+      EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0);
+
+  @override
+  Widget build(context) {
+    final mediaQuery = MediaQuery.of(context);
+    final height = mediaQuery.size.height;
+
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+          body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            height: height,
+            padding: containerPadding,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              SvgPicture.asset(
+                echnoPassword,
+                height: height * 0.5,
+              ),
+              Column(
+                children: [
+                  Text('Forgot Password!',
+                      style: Theme.of(context).textTheme.displaySmall),
+                  Text(
+                    'Please confirm your email address to reset your password...',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Column(
+                children: [
+                  TextFormField(
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.done,
+                    maxLines: 1,
+                    decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.mail_outline),
+                      labelText: 'Email ID',
+                      hintText: 'E-Mail',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                          (15.0),
+                        ),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Next'),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ),
+      )),
+    );
+  }
+}
