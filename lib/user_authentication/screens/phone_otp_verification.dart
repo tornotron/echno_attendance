10fb773f4c5027d9a91a3a922eb18e022eaf52d1
Abin K Johny <abinkjohny2011@gmail.com>
2023-06-11 21:35:57 +0530

    Created OTP Verification page for phone OTP

---
+++ lib/user_authentication/screens/phone_otp_verification.dart
@@ -0,0 +1,68 @@
+import 'package:echno_attendance/constants/image_string.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_otp_text_field/flutter_otp_text_field.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class PhoneOTPVerification extends StatelessWidget {
+  const PhoneOTPVerification({super.key});
+  static const EdgeInsetsGeometry containerPadding =
+      EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0);
+
+  @override
+  Widget build(context) {
+    final mediaQuery = MediaQuery.of(context);
+    final height = mediaQuery.size.height;
+
+    return Directionality(
+      textDirection: TextDirection.ltr,
+      child: Scaffold(
+          body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            width: double.infinity,
+            height: height,
+            padding: containerPadding,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+              SvgPicture.asset(
+                echnoOTP,
+                height: height * 0.5,
+              ),
+              Column(
+                children: [
+                  Text('OTP Verification',
+                      style: Theme.of(context).textTheme.displaySmall),
+                  Text(
+                    'Please enter the OTP you\'ve received via your phone...',
+                    style: Theme.of(context).textTheme.titleMedium,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10.0),
+              Column(
+                children: [
+                  OtpTextField(
+                    numberOfFields: 6,
+                    borderColor: Theme.of(context).primaryColor,
+                    showFieldAsBox: true,
+                    onCodeChanged: (String code) {},
+                    onSubmit: (String verificationCode) {},
+                  ),
+                  const SizedBox(height: 20.0),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      child: const Text('Confirm'),
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
+        ),
+      )),
+    );
+  }
+}
