b3b65ad22bc79b49580f5c049f4fe95f8d542e56
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-06-12 15:20:24 +0530

    Created a MockAuthProvider for testing

---
+++ test/auth_test.dart
@@ -0,0 +1,65 @@
+import 'package:echno_attendance/services/auth/auth_exceptions.dart';
+import 'package:echno_attendance/services/auth/auth_provider.dart';
+import 'package:echno_attendance/services/auth/auth_user.dart';
+import 'package:test/test.dart';
+
+void main() {}
+
+class NotInitializedException implements Exception {}
+
+class MockAuthProvider implements AuthProvider {
+  AuthUser? _user;
+  var _isInitialized = false;
+  bool get isInitialized => _isInitialized;
+
+  @override
+  Future<AuthUser> createUser({
+    required String email,
+    required String password,
+  }) async {
+    if (!_isInitialized) throw NotInitializedException();
+    await Future.delayed(const Duration(seconds: 1));
+    return logIn(
+      email: email,
+      password: password,
+    );
+  }
+
+  @override
+  AuthUser? get currentUser => _user;
+
+  @override
+  Future<void> initialize() async {
+    await Future.delayed(const Duration(seconds: 1));
+    _isInitialized = true;
+  }
+
+  @override
+  Future<AuthUser> logIn({
+    required String email,
+    required String password,
+  }) {
+    if (!_isInitialized) throw NotInitializedException();
+    if (email == 'foo@bar.com') throw UserNotFoundAuthException();
+    if (password == 'foobar') throw WrongPasswordAuthException();
+    const user = AuthUser(isemailVerified: false);
+    _user = user;
+    return Future.value(user);
+  }
+
+  @override
+  Future<void> logOut() async {
+    if (!_isInitialized) throw NotInitializedException();
+    if (_user == null) throw UserNotFoundAuthException();
+    await Future.delayed(const Duration(seconds: 1));
+    _user = null;
+  }
+
+  @override
+  Future<void> sendEmailVerification() async {
+    if (!_isInitialized) throw NotInitializedException();
+    if (_user == null) throw UserNotFoundAuthException();
+    const newUser = AuthUser(isemailVerified: true);
+    _user = newUser;
+  }
+}
