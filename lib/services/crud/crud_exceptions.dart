b419a5057a2251f5feb6779890ab7417a6068fdf
Abhijith Anandakrishnan <anandarkishnanabhijith@gmail.com>
2023-06-14 17:22:47 +0530

    Added CRUD service for attendance

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,11 @@
+class DatabaseNotOpenException implements Exception {}
+
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnabletoGetApplicationDocumentsDirectory implements Exception {}
+
+class CouldNotDeleteUserException implements Exception {}
+
+class UserAlreadyExistException implements Exception {}
+
+class UserNotFoundException implements Exception {}
