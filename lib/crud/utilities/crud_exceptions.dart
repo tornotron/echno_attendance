c004c0d25f12f3f31094166e662ed8a08bbe6d15
Abin K Johny <abinkjohny2011@gmail.com>
2023-08-22 10:47:52 +0530

    Defined exception handling in CRUD

---
+++ lib/crud/utilities/crud_exceptions.dart
@@ -0,0 +1,14 @@
+// Exceptions Class
+class DatabaseAlreadyOpenException implements Exception {}
+
+class Unabletogetdocumentsdirectory implements Exception {}
+
+class DatabaseNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotUpdateUser implements Exception {}
