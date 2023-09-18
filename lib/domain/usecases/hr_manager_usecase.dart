9c3b5ccbe79046a554658cef27096ca99e47f7be
Hrishikesh <hrishikesh0@gmail.com>
2023-09-18 23:48:40 +0530

    implemented update profile function

--- lib/domain/usecases/hr_manager_usecase.dart
+++ lib/domain/usecases/hr_manager_usecase.dart
@@ -1,27 +1,37 @@
 import 'package:echno_attendance/domain/usecases/user_creation.dart';
+import 'package:echno_attendance/domain/usecases/user_updation.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class InterfaceHr {
-  void createProfile(InterfaceUserCreation objUsercreation, String emName,
-      String emEmail, String emPassword) {}
+// ------------------INTERFACE-----------------------------
 
-  // void updateProfile() {}
+class InterfaceHr {
+  void createProfile(InterfaceUserCreation objUserCreation, String creationName,
+      String creationEmail, String creationPassword) {}
+
+  void updateProfile(InterfaceUserUpdation objUserUpdation, String updationName,
+      String updationEmail) {}
 
   // void readProfile() {}
 
   // void deleteProfile() {}
 }
+
+//--------------------------CLASS-----------------------------------
 
 class HrClass implements InterfaceHr {
   @override
-  void createProfile(InterfaceUserCreation objUsercreation, String emName,
-      String emEmail, String emPassword) {
-    Future<User?> empUser = objUsercreation.registerEmailPassword(
-        name: emName, email: emEmail, password: emPassword);
+  void createProfile(InterfaceUserCreation objUserCreation, String creationName,
+      String creationEmail, String creationPassword) {
+    objUserCreation.registerEmailPassword(
+        name: creationName, email: creationEmail, password: creationPassword);
   }
 
-  // @override
-  // void updateProfile() {}
+  @override
+  void updateProfile(InterfaceUserUpdation objUserUpdation, String updationName,
+      String updationEmail) {
+    objUserUpdation.userProfileUpdation(
+        name: updationName, email: updationEmail);
+  }
 
   // @override
   // void readProfile() {}
@@ -30,10 +40,9 @@ class HrClass implements InterfaceHr {
   // void deleteProfile() {}
 }
 
-void main()                                                                                                 //for testing
+void main() //for testing
 {
-  final hrUserHandling = HrClass().createProfile(EmUserCreation(), "jacob", "jacob@gmail.com", "1234");
- 
+  final hruser = HrClass().updateProfile(EmUserUpdation(), "HELLP", "huh");
 
   // hrClassWithEmUserHandling.updateProfile();
   // hrClassWithEmUserHandling.readProfile();
