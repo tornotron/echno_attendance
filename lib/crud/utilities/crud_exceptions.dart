e6e44e6b04e763c718290904ea84f96ed41f8d67
Abin K Johny <abinkjohny2011@gmail.com>
2023-10-20 15:36:56 +0530

    Initial Commit --> Exception handling in CRUD

---
+++ lib/crud/utilities/crud_exceptions.dart
@@ -0,0 +1,13 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class Unabletogetdocumentsdirectory implements Exception {}
+
+class DatabaseNotOpen implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class UserAlreadyExists implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotUpdateUser implements Exception {}
