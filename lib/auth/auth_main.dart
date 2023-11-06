7a563778263d5b9d5d7bb57814c5a62996666470
Abin K Johny <abinkjohny2011@gmail.com>
2023-11-06 23:38:00 +0530

    Created a main function for testing authentication

---
+++ lib/auth/auth_main.dart
@@ -0,0 +1,72 @@
+import 'package:echno_attendance/auth/screens/email_verification_screen.dart';
+import 'package:echno_attendance/auth/screens/home_screen.dart';
+import 'package:echno_attendance/auth/screens/login_screen.dart';
+import 'package:echno_attendance/auth/screens/registration_screen.dart';
+import 'package:echno_attendance/constants/custom_theme.dart';
+import 'package:echno_attendance/utilities/routes.dart';
+import 'package:echno_attendance/auth/services/auth_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // Initialize Firebase and the Flutter app.
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const EchnoTestApp());
+}
+
+class EchnoTestApp extends StatelessWidget {
+  const EchnoTestApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const NewEchnoHomePage(),
+      theme: EchnoCustomTheme.lightTheme,
+      darkTheme: EchnoCustomTheme.darkTheme,
+      themeMode: ThemeMode.system,
+      routes: {
+        loginRoute: (context) => const LoginScreen(),
+        registerRoute: (context) => const RegistrationScreen(),
+        verifyEmailRoute: (context) => const EmailVerification(),
+        homeRoute: (context) => const HomeScreen(),
+      },
+    );
+  }
+}
+
+class NewEchnoHomePage extends StatefulWidget {
+  const NewEchnoHomePage({super.key});
+
+  @override
+  State<NewEchnoHomePage> createState() => _NewEchnoHomePageState();
+}
+
+class _NewEchnoHomePageState extends State<NewEchnoHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: AuthService.firebase().initialize(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            final user = AuthService.firebase().currentUser;
+            if (user != null) {
+              if (user.isemailVerified) {
+                return const HomeScreen();
+              } else {
+                return const EmailVerification();
+              }
+            } else {
+              return const LoginScreen();
+            }
+          default:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+        }
+      },
+    );
+  }
+}
