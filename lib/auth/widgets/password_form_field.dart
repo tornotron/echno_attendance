e7d673433d1cca8ad35baa8979a64a063c04a9bc
Abin K Johny <abinkjohny2011@gmail.com>
2023-11-06 22:51:33 +0530

    Added controller constructor for PasswordTextField

--- lib/auth/widgets/password_form_field.dart
+++ lib/auth/widgets/password_form_field.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class PasswordTextField extends StatefulWidget {
   final String labelText;
   final String hintText;
+  final TextEditingController? controller;
 
   const PasswordTextField({
     Key? key,
     required this.labelText,
     required this.hintText,
+    this.controller,
   }) : super(key: key);
 
   @override
@@ -20,6 +22,7 @@ class _PasswordTextFieldState extends State<PasswordTextField> {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
+      controller: widget.controller,
       autocorrect: true,
       enableSuggestions: false,
       maxLines: 1,
