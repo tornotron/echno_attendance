a83b5f7659251bffea4812da718bedf8700fd64f
Abin K Johny <abinkjohny2011@gmail.com>
2023-11-09 20:48:03 +0530

    Defined auth events and auth states

---
+++ lib/auth/bloc/auth_state.dart
@@ -0,0 +1,29 @@
+import 'package:echno_attendance/auth/services/index.dart';
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+abstract class AuthState {
+  const AuthState();
+}
+
+class AuthLoadingState extends AuthState {
+  const AuthLoadingState();
+}
+
+class AuthLoggedInState extends AuthState {
+  final AuthUser user;
+  const AuthLoggedInState(this.user);
+}
+
+class AuthEmailNotVerifiedState extends AuthState {
+  const AuthEmailNotVerifiedState();
+}
+
+class AuthLoggedOutState extends AuthState {
+  const AuthLoggedOutState();
+}
+
+class AuthErrorState extends AuthState {
+  final Exception exception;
+  const AuthErrorState(this.exception);
+}
