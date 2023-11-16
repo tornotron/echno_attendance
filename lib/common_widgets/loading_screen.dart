e7ac0ef9f77929ea28b490e164f35c76e0d6c3b0
Abin K Johny <abinkjohny2011@gmail.com>
2023-11-16 22:54:19 +0530

    Designed LoadingScreens and LoadingScreenController

---
+++ lib/common_widgets/loading_screen.dart
@@ -0,0 +1,107 @@
+import 'dart:async';
+
+import 'package:echno_attendance/constants/colors_string.dart';
+import 'package:echno_attendance/utilities/loading_screen_controller.dart';
+import 'package:flutter/material.dart';
+
+class LoadingScreen {
+  factory LoadingScreen() => _shared;
+  static final LoadingScreen _shared = LoadingScreen._sharedInstance();
+  LoadingScreen._sharedInstance();
+
+  LoadingScreenController? controller;
+
+  void show({
+    required BuildContext context,
+    required String text,
+  }) {
+    if (controller?.update(text) ?? false) {
+      return;
+    } else {
+      controller = showOverlay(
+        context: context,
+        text: text,
+      );
+    }
+  }
+
+  void hide() {
+    controller?.close();
+    controller = null;
+  }
+
+  LoadingScreenController showOverlay({
+    required BuildContext context,
+    required String text,
+  }) {
+    final streamText = StreamController<String>();
+    streamText.add(text);
+
+    final state = Overlay.of(context);
+    final renderBox = context.findRenderObject() as RenderBox;
+    final size = renderBox.size;
+
+    final overlay = OverlayEntry(
+      builder: (context) {
+        bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
+        return Material(
+          color: echnoLightColor.withOpacity(0.5),
+          child: Center(
+            child: Container(
+                constraints: BoxConstraints(
+                  maxWidth: size.width * 0.8,
+                  maxHeight: size.height * 0.8,
+                  minWidth: size.width * 0.5,
+                ),
+                decoration: BoxDecoration(
+                  color: isDarkMode ? echnoGreyColor : echnoDarkColor,
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const SizedBox(height: 10),
+                        const LinearProgressIndicator(),
+                        const SizedBox(height: 20),
+                        StreamBuilder(
+                          stream: streamText.stream,
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              return Text(
+                                snapshot.data as String,
+                                style: Theme.of(context).textTheme.titleMedium,
+                                textAlign: TextAlign.center,
+                              );
+                            } else {
+                              return Container();
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+          ),
+        );
+      },
+    );
+
+    state.insert(overlay);
+
+    return LoadingScreenController(
+      close: () {
+        streamText.close();
+        overlay.remove();
+        return true;
+      },
+      update: (text) {
+        streamText.add(text);
+        return true;
+      },
+    );
+  }
+}
