d3ce932d7b03bdf5a88963a7d650147b448796a2
hrishikesh <hrishikeshajith0@gmail.com>
2023-12-18 20:30:51 +0530

    test for userhandling

---
+++ test/userhandle_test.dart
@@ -0,0 +1,65 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:echno_attendance/user/domain/firestore/userhandling_implementation.dart';
+
+void main() {
+  group('FirestoreUserServices', () {
+    late FirestoreUserServices firestoreUserServices;
+
+    setUp(() {
+      firestoreUserServices = FirestoreUserServices();
+    });
+
+    test('Create user should add a new user to Firestore', () async {
+      const userId = 'testUserId';
+      const name = 'Test User';
+      const email = 'test@example.com';
+      const phoneNumber = '1234567890';
+      const userRole = 'Employee';
+      const isActiveUser = true;
+
+      await firestoreUserServices.createUser(
+        userId: userId,
+        name: name,
+        email: email,
+        phoneNumber: phoneNumber,
+        userRole: userRole,
+        isActiveUser: isActiveUser,
+      );
+
+      final userData = await firestoreUserServices.readUser(userId: userId);
+
+      expect(userData['name'], equals(name));
+      expect(userData['email'], equals(email));
+      expect(userData['phoneNumber'], equals(phoneNumber));
+      expect(userData['userRole'], equals(userRole));
+      expect(userData['isActiveUser'], equals(isActiveUser));
+    });
+
+    test('Update user should modify user data in Firestore', () async {
+      const userId = 'testUserId';
+      const newName = 'Updated User Name';
+
+      await firestoreUserServices.updateUser(userId: userId, name: newName);
+
+      final updatedUserData =
+          await firestoreUserServices.readUser(userId: userId);
+
+      expect(updatedUserData['name'], equals(newName));
+    });
+
+    test('Delete user should remove user data from Firestore', () async {
+      const userId = 'testUserId';
+
+      await firestoreUserServices.deleteUser(userId: userId);
+
+      final deletedUserData =
+          await firestoreUserServices.readUser(userId: userId);
+
+      expect(deletedUserData['name'], isNull);
+      expect(deletedUserData['email'], isNull);
+      expect(deletedUserData['phoneNumber'], isNull);
+      expect(deletedUserData['userRole'], isNull);
+      expect(deletedUserData['isActiveUser'], isNull);
+    });
+  });
+}
