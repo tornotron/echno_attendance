1135179f7d8b1b02adee124022e3fd5e6012f381
Abin K Johny <abinkjohny2011@gmail.com>
2023-12-19 12:51:10 +0530

    Created leave_main.dart to test run the leave module

---
+++ lib/leave_module/leave_main.dart
@@ -0,0 +1,26 @@
+import 'package:echno_attendance/global_theme/custom_theme.dart';
+import 'package:echno_attendance/leave_module/screens/leave_application.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  // Initialize Firebase and the Flutter app.
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const EchnoTestApp());
+}
+
+class EchnoTestApp extends StatelessWidget {
+  const EchnoTestApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const LeaveApplicationScreen(),
+      theme: EchnoCustomTheme.lightTheme,
+      darkTheme: EchnoCustomTheme.darkTheme,
+      themeMode: ThemeMode.system,
+    );
+  }
+}
