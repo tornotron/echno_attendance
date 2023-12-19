520a44ed04fa24b38a2cd7157bd0ee52678c7b2e
Abin K Johny <abinkjohny2011@gmail.com>
2023-12-19 13:57:51 +0530

    Defined an LeaveProvider class

---
+++ lib/leave_module/services/leave_provider.dart
@@ -0,0 +1,27 @@
+abstract class LeaveProvider {
+  Future<void> applyForLeave({
+    required String uid,
+    required String employeeID,
+    required String employeeName,
+    required String appliedDate,
+    required String fromDate,
+    required String toDate,
+    required String? leaveType,
+    required String remarks,
+  });
+
+  Future<Map<String, dynamic>> searchEmployeeByUID({required String uid});
+
+  Stream<List<Map<String, dynamic>>> streamLeaveHistory({
+    required String? uid,
+  });
+
+  Future<void> cancelLeave({required String leaveId});
+
+  Stream<List<Map<String, dynamic>>> fetchLeaves();
+
+  Future<void> updateLeaveStatus({
+    required String leaveId,
+    required String newStatus,
+  });
+}
