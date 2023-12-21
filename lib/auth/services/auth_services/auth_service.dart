4a0f68234dc3465bf47ed761b9593086ec943c2a
Abin K Johny <abinkjohny2011@gmail.com>
2023-12-21 16:53:55 +0530

    Refactored lib/auth/services/firebase_auth_provider.dart -> lib/auth/services/auth_services/firebase_auth_provider.dart

--- lib/auth/services/auth_services/auth_service.dart
+++ lib/auth/services/auth_services/auth_service.dart
@@ -1,6 +1,6 @@
 import 'package:echno_attendance/auth/services/auth_services/auth_provider.dart';
 import 'package:echno_attendance/auth/services/auth_services/auth_user.dart';
-import 'package:echno_attendance/auth/services/firebase_auth_provider.dart';
+import 'package:echno_attendance/auth/services/auth_services/firebase_auth_provider.dart';
 
 class AuthService implements AuthProvider {
   final AuthProvider _provider;
