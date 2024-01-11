31b18ea7fd694def192aa964f64a547165b7ac87
hrishikesh <hrishikeshajith0@gmail.com>
2024-01-12 02:19:43 +0530

    added report type (daily and monthly)

--- lib/user/hr_screens/attendance_report/employee_attendancereport.dart
+++ lib/user/hr_screens/attendance_report/employee_attendancereport.dart
@@ -14,6 +14,8 @@ class AttendanceReportScreen extends StatefulWidget {
 }
 
 class _AttendanceReportScreenState extends State<AttendanceReportScreen> {
+  bool showMainContent = true;
+
   TextEditingController employeeIdController = TextEditingController();
   TextEditingController monthController = TextEditingController();
   TextEditingController yearController = TextEditingController();
@@ -28,6 +30,82 @@ class _AttendanceReportScreenState extends State<AttendanceReportScreen> {
     monthController.dispose();
     yearController.dispose();
     super.dispose();
+  }
+
+  void _showBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+        backgroundColor: Colors.white,
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 60,
+                  width: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: echnoBlueColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        textStyle: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          showMainContent = true;
+                        });
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        'Get Monthly Report',
+                        style: TextStyle(
+                          fontFamily: 'TT Chocolates',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 60,
+                  width: 200,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: echnoBlueColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        textStyle: const TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          showMainContent = false;
+                        });
+                        Navigator.pop(context);
+                      },
+                      child: const Text(
+                        'Get Daily Report',
+                        style: TextStyle(
+                          fontFamily: 'TT Chocolates',
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          );
+        });
   }
 
   @override
@@ -49,140 +127,191 @@ class _AttendanceReportScreenState extends State<AttendanceReportScreen> {
           color: Colors.white,
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 12.0, top: 20),
-            child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Get Attendance Report',
-                    style: EchnoTextTheme.lightTextTheme.displaySmall)),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 13.7),
-            child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text('Get Attendance Report',
-                    style: EchnoTextTheme.lightTextTheme.titleMedium)),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: employeeIdController,
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
-                        ),
-                        labelText: 'Emp ID',
-                        labelStyle: EchnoTextTheme.lightTextTheme.titleMedium),
-                  ),
+      body: showMainContent
+          ? Column(
+              children: [
+                const SizedBox(
+                  height: 10,
                 ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: DropdownSearch<String>(
-                    popupProps: const PopupProps.menu(
-                      showSelectedItems: true,
-                    ),
-                    items: const [
-                      "January",
-                      "February",
-                      "March",
-                      "April",
-                      "May",
-                      "June",
-                      "July",
-                      "August",
-                      "September",
-                      "October",
-                      "November",
-                      "December"
-                    ],
-                    dropdownDecoratorProps: DropDownDecoratorProps(
-                      dropdownSearchDecoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(15.0),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0, top: 20),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Monthly Attendance Report',
+                          style: EchnoTextTheme.lightTextTheme.displaySmall)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 13.7),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Get monthly attendance report',
+                          style: EchnoTextTheme.lightTextTheme.titleMedium)),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextFormField(
+                          controller: employeeIdController,
+                          decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              labelText: 'Emp ID',
+                              labelStyle:
+                                  EchnoTextTheme.lightTextTheme.titleMedium),
                         ),
-                        labelText: 'Month',
-                        labelStyle: EchnoTextTheme.lightTextTheme.titleMedium,
                       ),
                     ),
-                    onChanged: (value) {
-                      if (value == null) {
-                      } else {
-                        attendanceMonthfromUI = value;
-                      }
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: DropdownSearch<String>(
+                          popupProps: const PopupProps.menu(
+                            showSelectedItems: true,
+                          ),
+                          items: const [
+                            "January",
+                            "February",
+                            "March",
+                            "April",
+                            "May",
+                            "June",
+                            "July",
+                            "August",
+                            "September",
+                            "October",
+                            "November",
+                            "December"
+                          ],
+                          dropdownDecoratorProps: DropDownDecoratorProps(
+                            dropdownSearchDecoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15.0),
+                              ),
+                              labelText: 'Month',
+                              labelStyle:
+                                  EchnoTextTheme.lightTextTheme.titleMedium,
+                            ),
+                          ),
+                          onChanged: (value) {
+                            if (value == null) {
+                            } else {
+                              attendanceMonthfromUI = value;
+                            }
+                          },
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          keyboardType: TextInputType.number,
+                          inputFormatters: [
+                            FilteringTextInputFormatter.digitsOnly
+                          ],
+                          controller: yearController,
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            labelText: 'Year',
+                            labelStyle:
+                                EchnoTextTheme.lightTextTheme.titleMedium,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 250,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        employeeIdfromUI = employeeIdController.text;
+                        attendanceMonthfromUI = attendanceMonthfromUI;
+                        attendanceYearfromUI = yearController.text;
+                      });
                     },
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextField(
-                    controller: yearController,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: echnoBlueColor,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
                       ),
-                      labelText: 'Year',
-                      labelStyle: EchnoTextTheme.lightTextTheme.titleMedium,
+                      textStyle: const TextStyle(
+                          fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    child: const Text(
+                      'Submit',
+                      style: TextStyle(
+                        fontFamily: 'TT Chocolates',
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          SizedBox(
-            height: 50,
-            width: 250,
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  employeeIdfromUI = employeeIdController.text;
-                  attendanceMonthfromUI = attendanceMonthfromUI;
-                  attendanceYearfromUI = yearController.text;
-                });
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: echnoBlueColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15),
+                const SizedBox(
+                  height: 10,
                 ),
-                textStyle:
-                    const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
-              ),
-              child: const Text(
-                'Submit',
-                style: TextStyle(
-                  fontFamily: 'TT Chocolates',
-                  color: Colors.white,
+                AttendanceCard(
+                  employeeId: employeeIdfromUI,
+                  attendanceMonth: attendanceMonthfromUI,
+                  attYear: attendanceYearfromUI,
                 ),
-              ),
+              ],
+            )
+          : Column(
+              children: [
+                const SizedBox(
+                  height: 10,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 12.0, top: 20),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Daily Attendance Report',
+                          style: EchnoTextTheme.lightTextTheme.displaySmall)),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 13.7),
+                  child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text('Get daily attendance report',
+                          style: EchnoTextTheme.lightTextTheme.titleMedium)),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
+      floatingActionButton: ElevatedButton(
+        onPressed: () {
+          _showBottomSheet(context);
+        },
+        style: ElevatedButton.styleFrom(
+          backgroundColor: echnoBlueLightColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15),
           ),
-          const SizedBox(
-            height: 10,
+          textStyle: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+        ),
+        child: const Text(
+          "Change report type",
+          style: TextStyle(
+            fontFamily: 'TT Chocolates',
+            color: echnoBlueColor,
           ),
-          AttendanceCard(
-            employeeId: employeeIdfromUI,
-            attendanceMonth: attendanceMonthfromUI,
-            attYear: attendanceYearfromUI,
-          ),
-        ],
+        ),
       ),
       backgroundColor: Colors.white,
     );
