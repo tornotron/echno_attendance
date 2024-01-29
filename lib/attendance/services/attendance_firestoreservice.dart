5e467ac3a89e92eb70471142cd49dc60418665b6
hrishikesh <hrishikeshajith0@gmail.com>
2024-01-29 13:04:33 +0530

    firestore implementation for insertion and fetching for monthly

---
+++ lib/attendance/services/attendance_firestoreservice.dart
@@ -0,0 +1,169 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:echno_attendance/attendance/services/attendance_abstarct.dart';
+import 'package:echno_attendance/logger.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:logger/logger.dart';
+
+class AttendanceFirestoreService implements AttendanceHandleProvider {
+  final logs = logger(AttendanceFirestoreService, Level.info);
+
+  @override
+  Future<void> insertIntoDatabase({
+    required String employeeId,
+    required String employeeName,
+    required String attendanceDate,
+    required String attendanceMonth,
+    required String attendanceTime,
+    required String attendanceStatus,
+    required String siteName,
+  }) async {
+    try {
+      List<int> dateComponents =
+          attendanceDate.split('-').map((e) => int.parse(e)).toList();
+      int year = dateComponents[0];
+      int month = dateComponents[1];
+      int day = dateComponents[2];
+
+      List<int> timeComponents =
+          attendanceTime.split(':').map((e) => int.parse(e)).toList();
+      int hour = timeComponents[0];
+      int minute = timeComponents[1];
+      int second = timeComponents[2];
+
+      DateTime combinedDate = DateTime(year, month, day, hour, minute, second);
+
+      CollectionReference firestoreattendance =
+          FirebaseFirestore.instance.collection('attendance');
+      await firestoreattendance.doc(employeeId).set(
+        {
+          "employee_id": employeeId,
+          "employee_name": employeeName,
+        },
+      );
+      firestoreattendance
+          .doc(employeeId)
+          .collection('attendancedate')
+          .doc(attendanceDate)
+          .set(
+        {
+          "attendance_date": combinedDate,
+          "attendance_month": attendanceMonth,
+          "attendance_time": attendanceTime,
+          "attendance_status": attendanceStatus,
+          "site_name": siteName,
+        },
+      );
+    } on FirebaseException catch (error) {
+      logs.e('Firebase Exception: ${error.message}');
+    } catch (e) {
+      logs.e('Other Exception: $e');
+    }
+  }
+
+  @override
+  Future<List<Map<String, String>>> fetchFromDatabase(
+      {required String employeeId,
+      required String attendanceMonth,
+      required String attYear}) async {
+    final attYearint = int.parse(attYear);
+    final Map<String, int> monthdayMap = {
+      'January': 31,
+      'February': await daysInFebruary(attYearint),
+      'March': 31,
+      'April': 30,
+      'May': 31,
+      'June': 30,
+      'July': 31,
+      'August': 31,
+      'September': 30,
+      'October': 31,
+      'November': 30,
+      'December': 31,
+    };
+    final Map<String, int> monthMap = {
+      'January': 1,
+      'February': 2,
+      'March': 3,
+      'April': 4,
+      'May': 5,
+      'June': 6,
+      'July': 7,
+      'August': 8,
+      'September': 9,
+      'October': 10,
+      'November': 11,
+      'December': 12,
+    };
+    try {
+      final attMonthint = monthMap[attendanceMonth];
+
+      final endMonthday = monthdayMap[attendanceMonth];
+
+      final startDate = DateTime(attYearint, attMonthint!, 1);
+      final endDate =
+          DateTime(attYearint, attMonthint, endMonthday!, 23, 59, 59);
+
+      List<Map<String, dynamic>> attendanceList = [];
+      CollectionReference firestoreattendance =
+          FirebaseFirestore.instance.collection('attendance');
+      final snapshot = firestoreattendance.doc(employeeId);
+      final dateData = snapshot.collection('attendancedate');
+      QuerySnapshot querySnapshot = await dateData
+          .where('attendance_date', isGreaterThanOrEqualTo: startDate)
+          .where('attendance_date', isLessThanOrEqualTo: endDate)
+          .get();
+
+      querySnapshot.docs.forEach((DocumentSnapshot document) {
+        Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+        Timestamp t =data['attendance_date'];
+        DateTime d = t.toDate();
+        final dateString = d.toString();
+        data['attendance_date'] = dateString;
+        attendanceList.add(data);
+      });
+      List<Map<String, String>> attendanceformattted = attendanceList
+          .map(
+              (row) => row.map((key, value) => MapEntry(key, value.toString())))
+          .toList();
+
+      return attendanceformattted;
+    } on FirebaseException catch (error) {
+      logs.e('Firebase Exception: ${error.message}');
+    } catch (e) {
+      logs.e('Other Exception: $e');
+    }
+    return [];
+  }
+
+  Future<int> daysInFebruary(int attYear) async {
+    return (attYear % 4 == 0 && (attYear % 100 != 0 || attYear % 400 == 0))
+        ? 29
+        : 28;
+  }
+
+  @override
+  Future<List<Map<String, String>>> fetchFromDatabaseDaily(
+      {required String siteName, required String date}) async {
+
+
+    try {
+      CollectionReference firestoreattendance =
+          FirebaseFirestore.instance.collection('attendance');
+    } on FirebaseException catch (error) {
+      logs.e('Firebase Exception: ${error.message}');
+    } catch (e) {
+      logs.e('Other Exception: $e');
+    }
+
+    return [];
+  }
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  final res = await AttendanceFirestoreService().fetchFromDatabase(
+      employeeId: "EMP-300", attendanceMonth: "January", attYear: "2024");
+  print(res);
+}
