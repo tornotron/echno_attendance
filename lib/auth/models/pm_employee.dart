9818963c3c0c6dca5706f982c25b998df5cd5713
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-03-05 00:19:26 +0530

    Removed unused import from read employee

--- lib/auth/models/pm_employee.dart
+++ lib/auth/models/pm_employee.dart
@@ -1,4 +1,5 @@
 import 'package:echno_attendance/auth/models/employee.dart';
+import 'package:echno_attendance/employee/services/crud/read_employee.dart';
 
 class PmEmployee extends Employee {
   PmEmployee({required super.user});
