709d5bb9385a59e12cfdb190bd3944869b82fbfe
Abin K Johny <abinkjohny2011@gmail.com>
2024-03-25 17:28:50 +0530

    Update theme for leave_register.dart

--- lib/leave_module/screens/leave_register.dart
+++ lib/leave_module/screens/leave_register.dart
@@ -1,23 +1,23 @@
-import 'package:echno_attendance/constants/colors_string.dart';
+import 'package:echno_attendance/common_widgets/custom_app_bar.dart';
+import 'package:echno_attendance/constants/colors.dart';
 import 'package:echno_attendance/constants/leave_module_strings.dart';
+import 'package:echno_attendance/constants/sizes.dart';
 import 'package:echno_attendance/employee/models/employee.dart';
 import 'package:echno_attendance/employee/services/employee_service.dart';
 import 'package:echno_attendance/employee/utilities/employee_role.dart';
 import 'package:echno_attendance/leave_module/widgets/register_stream_builder.dart';
+import 'package:echno_attendance/utilities/helpers/helper_functions.dart';
+import 'package:echno_attendance/utilities/styles/padding_style.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LeaveRegisterScreen extends StatefulWidget {
   const LeaveRegisterScreen({super.key});
-  static const EdgeInsetsGeometry containerPadding =
-      EdgeInsets.symmetric(vertical: 30.0, horizontal: 30.0);
 
   @override
   State<LeaveRegisterScreen> createState() => LeaveRegisterScreenState();
 }
 
 class LeaveRegisterScreenState extends State<LeaveRegisterScreen> {
-  get isDarkMode => Theme.of(context).brightness == Brightness.dark;
   final TextEditingController _searchController = TextEditingController();
   late final Employee currentEmployee;
 
@@ -36,83 +36,79 @@ class LeaveRegisterScreenState extends State<LeaveRegisterScreen> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = Scaffold(
-      appBar: AppBar(
-        systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: isDarkMode ? echnoLightBlueColor : echnoBlueColor),
-        backgroundColor: isDarkMode ? echnoLightBlueColor : echnoBlueColor,
-        title: const Text(leaveRegisterAppBarTitle),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios_new),
-          onPressed: () {
-            Navigator.pop(context);
-          },
+    final isDark = EchnoHelperFunctions.isDarkMode(context);
+
+    Widget content = Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(
+          leaveRegisterScreenTitle,
+          style: Theme.of(context).textTheme.displaySmall,
+          textAlign: TextAlign.left,
         ),
-      ),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: LeaveRegisterScreen.containerPadding,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Text(
-                  leaveRegisterScreenTitle,
-                  style: Theme.of(context).textTheme.displaySmall,
-                  textAlign: TextAlign.left,
-                ),
-                Text(
-                  leaveRegisterSubtitle,
-                  style: Theme.of(context).textTheme.titleMedium,
-                  textAlign: TextAlign.left,
-                ),
-                const SizedBox(height: 15.0),
-                TextFormField(
-                  controller: _searchController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    labelText: leaveRegisterFilterFieldLabel,
-                    hintText: leaveRegisterFilterFieldHint,
-                    suffixIcon: IconButton(
-                      icon: const Icon(Icons.clear),
-                      onPressed: () {
-                        setState(() {
-                          _searchController.clear();
-                        });
-                      },
-                    ),
-                  ),
-                  onChanged: (value) {
-                    // Trigger filtering when the user types
-                    setState(() {});
-                  },
-                ),
-                const SizedBox(height: 10.0),
-                const Divider(height: 3.0),
-              ],
+        Text(
+          leaveRegisterSubtitle,
+          style: Theme.of(context).textTheme.titleMedium,
+          textAlign: TextAlign.left,
+        ),
+        const SizedBox(height: EchnoSize.spaceBtwSections),
+        TextFormField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(EchnoSize.borderRadiusLg),
+            ),
+            labelText: leaveRegisterFilterFieldLabel,
+            hintText: leaveRegisterFilterFieldHint,
+            suffixIcon: IconButton(
+              icon: const Icon(Icons.clear),
+              onPressed: () {
+                setState(() {
+                  _searchController.clear();
+                });
+              },
             ),
           ),
-          leaveRegisterStreamBuilder(
-            isDarkMode: isDarkMode,
-            searchController: _searchController,
-            context: context,
-          ),
-        ],
-      ),
+          onChanged: (value) {
+            // Trigger filtering when the user types
+            setState(() {});
+          },
+        ),
+        const SizedBox(height: EchnoSize.spaceBtwItems),
+        const Divider(height: EchnoSize.dividerHeight),
+        leaveRegisterStreamBuilder(
+          isDarkMode: isDark,
+          searchController: _searchController,
+          context: context,
+        ),
+      ],
     );
     if (currentEmployee.employeeRole != EmployeeRole.hr) {
-      content = Scaffold(
-        body: Center(
-          child: Text(
-            'You are not authorized to access this Page. Please contact HR',
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
+      content = Center(
+        child: Text(
+          'You are not authorized to access this Page. Please contact HR',
+          style: Theme.of(context).textTheme.titleMedium,
+          textAlign: TextAlign.center,
         ),
       );
     }
-    return content;
+    return Scaffold(
+      appBar: EchnoAppBar(
+        leadingIcon: Icons.arrow_back_ios_new,
+        leadingOnPressed: () {
+          Navigator.pop(context);
+        },
+        title: Text(
+          'Leave Register',
+          style: Theme.of(context).textTheme.headlineSmall?.apply(
+                color: isDark ? EchnoColors.black : EchnoColors.white,
+              ),
+        ),
+      ),
+      body: Padding(
+        padding: CustomPaddingStyle.defaultPaddingWithAppbar,
+        child: content,
+      ),
+    );
   }
 }
--- lib/leave_module/widgets/leave_register_card.dart
+++ lib/leave_module/widgets/leave_register_card.dart
@@ -1,5 +1,6 @@
-import 'package:echno_attendance/constants/colors_string.dart';
+import 'package:echno_attendance/constants/colors.dart';
 import 'package:echno_attendance/constants/leave_module_strings.dart';
+import 'package:echno_attendance/constants/sizes.dart';
 import 'package:echno_attendance/leave_module/models/leave_model.dart';
 import 'package:echno_attendance/leave_module/screens/leave_approval_screen.dart';
 import 'package:echno_attendance/leave_module/utilities/leave_status.dart';
@@ -12,13 +13,26 @@ Widget leaveRow(
   bool isDarkMode,
   BuildContext context,
 ) {
+  final String startDate = DateFormat('dd-MM-yyyy').format(leave.fromDate);
+  final String endDate = DateFormat('dd-MM-yyyy').format(leave.toDate);
+  final String appliedDate = DateFormat('dd-MM-yyyy').format(leave.appliedDate);
   final leaveRegisterCard = Container(
-    height: 200,
-    margin: const EdgeInsets.only(left: 25.0, right: 25.0),
+    height: 170.0,
+    margin: const EdgeInsets.only(left: 16.0, right: 16.0),
     decoration: BoxDecoration(
-      color: isDarkMode ? echnoLightBlueColor : echnoBlueColor,
+      color: isDarkMode
+          ? EchnoColors.black.withOpacity(0.9)
+          : EchnoColors.lightCard,
       shape: BoxShape.rectangle,
-      borderRadius: BorderRadius.circular(8.0),
+      borderRadius: BorderRadius.circular(EchnoSize.borderRadiusMd),
+      boxShadow: <BoxShadow>[
+        BoxShadow(
+          color: isDarkMode ? EchnoColors.darkShadow : EchnoColors.lightShadow,
+          blurRadius: 10.0,
+          spreadRadius: 2.0,
+          offset: const Offset(0, 5),
+        ),
+      ],
     ),
     child: InkWell(
       onTap: () {
@@ -38,75 +52,85 @@ Widget leaveRow(
           children: <Widget>[
             Text(
               leave.employeeName,
-              style: const TextStyle(
-                color: echnoDarkColor,
-                fontFamily: 'TT Chocolates Bold',
-                fontWeight: FontWeight.w600,
-                fontSize: 18.0,
-              ),
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w700,
+                    color: isDarkMode
+                        ? EchnoColors.leaveText
+                        : EchnoColors.leaveText,
+                  ),
             ),
             const SizedBox(height: 5.0),
             Text(
               getLeaveTypeName(leave.leaveType),
-              style: const TextStyle(
-                color: echnoDarkColor,
-                fontFamily: 'TT Chocolates Bold',
-                fontWeight: FontWeight.w400,
-                fontSize: 18.0,
-              ),
+              style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                    fontSize: 17.0,
+                    fontWeight: FontWeight.normal,
+                    color: isDarkMode
+                        ? EchnoColors.leaveText
+                        : EchnoColors.leaveText,
+                  ),
             ),
             const SizedBox(height: 5.0),
             Text(
-              '$leaveRegisterDurationFieldLabel ${DateFormat('dd-MM-yyyy').format(leave.fromDate)}  -  ${DateFormat('dd-MM-yyyy').format(leave.toDate)}',
-              style: const TextStyle(
-                color: echnoDarkColor,
-                fontFamily: 'TT Chocolates Bold',
-                fontWeight: FontWeight.w400,
-                fontSize: 15.0,
-              ),
+              '$leaveRegisterDurationFieldLabel $startDate  --  $endDate',
+              style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w400,
+                    color: isDarkMode
+                        ? EchnoColors.leaveText
+                        : EchnoColors.leaveText,
+                  ),
             ),
             const SizedBox(height: 5.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
-                const Text(leaveRegisterStatusFieldLabel,
-                    style: TextStyle(
-                      color: echnoDarkColor,
-                      fontFamily: 'TT Chocolates Bold',
-                      fontWeight: FontWeight.w400,
-                      fontSize: 14.0,
-                    )),
-                const Padding(
-                  padding: EdgeInsets.all(2.0),
+                Text(
+                  leaveRegisterStatusFieldLabel,
+                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w300,
+                        color: isDarkMode
+                            ? EchnoColors.leaveText
+                            : EchnoColors.leaveText,
+                      ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(2.0),
                   child: Icon(
                     Icons.calendar_today,
                     size: 14.0,
-                    color: echnoDarkColor,
+                    color: isDarkMode
+                        ? EchnoColors.leaveText
+                        : EchnoColors.leaveText,
                   ),
                 ),
                 Text(
                   leave.isCancelled == false
                       ? getLeaveStatusName(leave.leaveStatus)
                       : leaveRegisterCancelledStatus,
-                  style: const TextStyle(
-                    color: echnoDarkColor,
-                    fontFamily: 'TT Chocolates Bold',
-                    fontWeight: FontWeight.w400,
-                    fontSize: 14.0,
-                  ),
+                  style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w400,
+                        color: isDarkMode
+                            ? EchnoColors.leaveText
+                            : EchnoColors.leaveText,
+                      ),
                 ),
               ],
             ),
             const SizedBox(height: 10.0),
             Text(
-              "$leaveRegisterAppliedFieldLabel ${DateFormat('dd-MM-yyyy').format(leave.appliedDate)}",
-              style: const TextStyle(
-                color: echnoDarkColor,
-                fontFamily: 'TT Chocolates Bold',
-                fontWeight: FontWeight.w600,
-                fontSize: 14.0,
-                letterSpacing: 0.5,
-              ),
+              "$leaveRegisterAppliedFieldLabel $appliedDate",
+              style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                    fontSize: 13.0,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 1.0,
+                    color: isDarkMode
+                        ? EchnoColors.leaveText
+                        : EchnoColors.leaveText,
+                  ),
             ),
           ],
         ),
